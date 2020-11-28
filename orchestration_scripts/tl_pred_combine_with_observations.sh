656dc4d7f335b9ad85ed43d1f9b306e526ba3bdc
Raman Samusevich <raman.samusevich@honeywell.com>
2020-11-28 13:57:15 +0100

    Merge branch 'master' of C:\Users\h184146\Documents\projects\lyft_trajectories with conflicts.

---
+++ orchestration_scripts/tl_pred_combine_with_observations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python -m lyft_trajectories.data_preprocessing.tl_pred_combined_with_observations --events-basename "tl_events_df_test_0" --predictions-basename "tl_pred_test_0"
+#python -m lyft_trajectories.data_preprocessing.tl_pred_combined_with_observations --events-basename "tl_events_df_validate_0" --predictions-basename "tl_pred_validate_0"
+python -m lyft_trajectories.data_preprocessing.tl_pred_combined_with_observations --events-basename "tl_events_df_test_0" --predictions-basename "tl_pred_test_1"
+#python -m lyft_trajectories.data_preprocessing.tl_pred_combined_with_observations --events-basename "tl_events_df_validate_0" --predictions-basename "tl_pred_validate_1"
+#python -m lyft_trajectories.data_preprocessing.tl_pred_combined_with_observations --events-basename "tl_events_df_train_full_0" --predictions-basename "tl_pred_0"
+#python -m lyft_trajectories.data_preprocessing.tl_pred_combined_with_observations --events-basename "tl_events_df_train_full_1" --predictions-basename "tl_pred_1"
